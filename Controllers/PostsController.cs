c43be2ec9307d886c3bf5e0fb4ed14e4d2a347d0
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2025-04-16 22:20:19 -0300

    feat(posts): agregar controlador con endpoints CRUD para la entidad Post

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -22,11 +22,59 @@ public class PostsController : ControllerBase
     return await _context.Posts.ToListAsync();
   }
 
-  [HttpPost]
-  public async Task<ActionResult<Post>> CrearPost(Post post)
+  [HttpGet("{id}")]
+  public async Task<ActionResult<Post>> GetPost(int id)
   {
+    var post = await _context.Posts.FindAsync(id);
+    if (post == null)
+    {
+      return NotFound();
+    }
+    return post;
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<Post>> CreatePost(Post post)
+  {
+    post.FechaCreacion = DateTime.Now;
     _context.Posts.Add(post);
     await _context.SaveChangesAsync();
-    return CreatedAtAction(nameof(GetPosts), new { id = post.Id }, post);
+
+    return CreatedAtAction(nameof(GetPost), new { id = post.Id }, post);
+  }
+
+  [HttpPut("{id}")]
+  public async Task<IActionResult> UpdatePost(int id, Post updatedPost)
+  {
+    if (id != updatedPost.Id)
+    {
+      return BadRequest();
+    }
+
+    var existingPost = await _context.Posts.FindAsync(id);
+    if (existingPost == null)
+    {
+      return NotFound();
+    }
+
+    existingPost.Titulo = updatedPost.Titulo;
+    existingPost.Contenido = updatedPost.Contenido;
+
+    await _context.SaveChangesAsync();
+    return NoContent();
+  }
+
+  [HttpDelete("{id}")]
+  public async Task<IActionResult> DeletePost(int id)
+  {
+    var post = await _context.Posts.FindAsync(id);
+    if (post == null)
+    {
+      return NotFound();
+    }
+
+    _context.Posts.Remove(post);
+    await _context.SaveChangesAsync();
+    return NoContent();
   }
 }
